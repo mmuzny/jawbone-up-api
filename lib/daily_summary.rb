81db7d0e595cdbc884ad06d6dd25feb010300529
Niklas Lindblad <niklas@lindblad.info>
2013-07-13 00:49:30 +0200

    Update DailySummary to conform to new data format

--- lib/daily_summary.rb
+++ lib/daily_summary.rb
@@ -9,25 +9,23 @@ module Jawbone
     end
     
     def steps
-      @data["move_steps"]
+      @data["move"]["bg_steps"]
     end
     
     def kilometers
-      @data["move_kilometers"]
+      @data["move"]["distance"]
     end
     
     def workout?
-      !@data["move_summary"]["workouts"].empty?
+      !@data["move"]["goals"]["workout_time"].empty?
     end
     
     def sleep_total
-      @data["sleep_summary"]["total"]
+      @data["sleep"]["goals"]["total"]
     end
     
     def sleep_quality
-      return nil if @data["sleep_summary"]["sleeps"].empty?
-      sleeps = @data["sleep_summary"]["sleeps"].map { |s| [s["details"]["duration"], s["details"]["quality"]]}
-      sleeps.sort_by { |s| s[0] }.first[1]
+      return @data["sleep"]["qualities"]
     end
   end
   
